da4bf15e3d087c4a3ff430ef92202c95f9cb1a7d
Vikas <154679615+coder-01-all@users.noreply.github.com>
2024-06-09 16:19:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ function login(username, password, isAutoLogin) {
                     localStorage.setItem('username', username);
                     localStorage.setItem('password', password);
                 }
-                alert('Login successful!');
                 console.log('Redirecting to:', user.redirectUrl);
                 window.location.href = user.redirectUrl;
             } else {
